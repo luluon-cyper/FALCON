a9a5f85f03028eedd8c5c93152d6ae6f692a192c
june2605 <duy02062005@gmail.com>
2026-03-23 15:01:16 +0700

    Done

--- run_esign.py
+++ run_esign.py
@@ -3,25 +3,46 @@ import sys
 import threading
 import time
 import subprocess
+import tempfile
+import shutil
 import streamlit.web.cli as stcli
+
+APP_PORT = 8501
 
 def open_browser():
     time.sleep(3)
-    url = "http://localhost:8501"
+    url = f"http://localhost:{APP_PORT}"
+    browser_process = None
     
+    temp_profile_dir = os.path.join(tempfile.gettempdir(), "esign_browser_profile")
+
     try:
-        edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+        pf86 = os.environ.get('PROGRAMFILES(X86)', r'C:\Program Files (x86)')
+        pf = os.environ.get('PROGRAMFILES', r'C:\Program Files')
+        
+        edge_path = os.path.join(pf86, r"Microsoft\Edge\Application\msedge.exe")
+        chrome_path = os.path.join(pf, r"Google\Chrome\Application\chrome.exe")
+        chrome_path_x86 = os.path.join(pf86, r"Google\Chrome\Application\chrome.exe")
+        
         if os.path.exists(edge_path):
-            subprocess.Popen([edge_path, f"--app={url}"])
+            browser_process = subprocess.Popen([edge_path, f"--app={url}", f"--user-data-dir={temp_profile_dir}"])
+        elif os.path.exists(chrome_path):
+            browser_process = subprocess.Popen([chrome_path, f"--app={url}", f"--user-data-dir={temp_profile_dir}"])
+        elif os.path.exists(chrome_path_x86):
+            browser_process = subprocess.Popen([chrome_path_x86, f"--app={url}", f"--user-data-dir={temp_profile_dir}"])
         else:
-            chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-            if os.path.exists(chrome_path):
-                subprocess.Popen([chrome_path, f"--app={url}"])
-            else:
-                import webbrowser
-                webbrowser.open(url)
-    except Exception as e:
+            import webbrowser
+            webbrowser.open(url)
+    except Exception:
         pass
+
+    if browser_process:
+        browser_process.wait()
+        try:
+            shutil.rmtree(temp_profile_dir, ignore_errors=True)
+        except Exception:
+            pass
+        os._exit(0)
 
 def run_streamlit():
     if getattr(sys, 'frozen', False):
@@ -36,7 +57,8 @@ def run_streamlit():
         "run", 
         app_path, 
         "--server.headless=true", 
-        "--global.developmentMode=false"
+        "--global.developmentMode=false",
+        "--server.port", str(APP_PORT)
     ]
     
     sys.exit(stcli.main())
